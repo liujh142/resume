f5fcc2a129ee34a89767d9fa36bfcba2fe121b61
XY <1425662757@qq.com>
2020-08-25 17:07:33 +0800

    8.25

--- js/index.js
+++ js/index.js
@@ -1,11 +1,25 @@
-var navBar = document.querySelector('.nav-bar')
-window.addEventListener('scroll', function(e){
+function $(selector){
+    return document.querySelector(selector);
+}
+window.addEventListener('scroll',function(){
     if(document.documentElement.scrollTop > 60){
-        navBar.classList.add('nav-bar-bg');
+        $('.nav-bar').classList.add('nav-bar-bg')
+        $('header').setAttribute('style','min-height:40px')
     }else{
-        navBar.classList.remove('nav-bar-bg')
+        $('.nav-bar').classList.remove('nav-bar-bg')
+        $('header').setAttribute('style','min-height:0px');
     }
-    // if(document.documentElement.scrollTop < 60){
-    //     navBar.classList.remove('nav-bar-bg')
-    // }
-  })
+})
+$('.scroll-top').onclick = function(){
+    var scrollTop = setInterval(function(){
+        document.documentElement.scrollTop = document.documentElement.scrollTop  -  5;
+        if(document.documentElement.scrollTop == 0){
+            clearInterval(scrollTop);
+        }
+    },1)
+}
+$('.menu-button').onclick = function(){
+    $('.sidebar').setAttribute('style','right:0px;display:block');  
+    $('.overlay').setAttribute('style','display:block');
+    $('html').setAttribute('style','overflow: hidden')
+}
